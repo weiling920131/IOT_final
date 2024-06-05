6bbf4cb64dc96ce95d1487487200c74b10d0d051
weiling920131 <weiling920131@gmail.com>
2024-06-05 18:18:08 +0800

    no linebot

--- DAIcopy.py
+++ DAIcopy.py
@@ -3,26 +3,26 @@ import DAN
 # import crawl_weather_V8 as cw
 import os
 import csv
-from STT import STTAgent 
+# from STT import STTAgent 
 
 ServerURL = 'https://2.iottalk.tw/'      #with non-secure connection
 #ServerURL = 'https://DomainName' #with SSL connection
-Reg_addr = '170.2' #if None, Reg_addr = MAC address
+Reg_addr = '170.2.1' #if None, Reg_addr = MAC address
 
 DAN.profile['dm_name'] = 'Dummy_Device'
-DAN.profile['df_list']=['Dummy_Sensor']
-DAN.profile['d_name']= '206_Dummy_Device'
+DAN.profile['df_list']=['Dummy_Control']
+DAN.profile['d_name']= '206_Dummy_Device_ODF'
 
 DAN.device_registration_with_retry(ServerURL, Reg_addr)
 #DAN.deregister()  #if you want to deregister this device, uncomment this line
 #exit()            #if you want to deregister this device, uncomment this line
-stt = STTAgent()
+# stt = STTAgent()
 while True:
     try:
-        text = stt.run()
+        text = DAN.pull('Dummy_Control')
+        # print(text[0])
         if text is not None:
-            print(text)
-            DAN.push('Dummy_Sensor', text)
+            print(text[0])
     except Exception as e:
         print(e)
         if str(e).find('mac_addr not found:') != -1:
